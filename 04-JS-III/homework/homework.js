3360863d224fa90076a2e939fc1fdea015ebc9e4
Bauti2018 <bautinavarrete@hotmail.com>
2023-01-31 23:12:29 -0300

    tp 3 listo

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -165,7 +165,11 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  var xd = String(n)
+  if (xd[0] == 9)
+    return true
+  else
+    return false
 }
 
 
@@ -179,8 +183,6 @@ function todosIguales(arreglo) {
       else
         return true
     }
-
-  
 } 
 
 
@@ -224,6 +226,17 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var nuevo=[]
+  var numx = numero
+   for (var i = 0; i < 10;i++){ 
+     numx = numx + 2
+     if (numx === i){
+      nuevo = 'Se interrumpió la ejecución'
+      break
+   }
+     nuevo.push(numx)
+  }
+   return nuevo
 }
 
 
@@ -234,6 +247,17 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var nuevo=[]
+  var numx = numero
+   for (var i = 0; i < 10;i++){ 
+    if (i === 5){
+      numx - 2  
+      continue
+    } 
+    numx = numx + 2
+    nuevo.push(numx)
+  }
+   return nuevo
 }
 
 
